06a723aea654ec5e6923a45c4acb8ec025f1ad43
Darko <darkopavikevik@gmail.com>
2025-06-04 15:59:06 +0200

    Security changes FIXED

--- src/main/java/com/example/gotogether/security/JwtUtil.java
+++ src/main/java/com/example/gotogether/security/JwtUtil.java
@@ -4,6 +4,7 @@ import com.example.gotogether.model.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -14,12 +15,14 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private final String SECRET_KEY = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+
+    @Value("${jwt.secret}")
+    private String secretKey;
     private final long ACCESS_TOKEN_EXPIRATION = 1000 * 60 * 60; // 1 hour
     private final long REFRESH_TOKEN_EXPIRATION = 1000 * 60 * 60 * 24 * 7; // 7 days
 
     private Key getSigningKey() {
-        byte[] keyBytes = Base64.getDecoder().decode(SECRET_KEY);
+        byte[] keyBytes = Base64.getDecoder().decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
